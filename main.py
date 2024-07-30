1d4ddcb244e626d613a52588d59bcc3ed854ab7b
Iaroslav Plaksin <Plak.I.A@yandex.ru>
2024-07-31 01:45:03 +0300

    Update about page

--- main.py
+++ main.py
@@ -532,7 +532,7 @@ class AboutForm(QMainWindow, about.Ui_MainWindow):
     def __init__(self, *args):
         super().__init__()
         self.setupUi(self)
-        self.toolButton_mail_author.clicked.connect(lambda: webbrowser.open('mailto:Batov.I.A@yandex.ru'))
+        self.toolButton_mail_author.clicked.connect(lambda: webbrowser.open('mailto:Plak.I.A@yandex.ru'))
         self.toolButton_mail_publisher.clicked.connect(lambda: webbrowser.open('mailto:plaksin.network@gmail.com'))
         self.toolButton_license.clicked.connect(
             lambda: webbrowser.open('https://github.com/PlakIA/CryptoDog/blob/main/LICENSE'))
